a081a93e22080545e04844051cb1a9fe3fc8b2e0
Gopala Krishna Chennu <48098312+gopalakrishnachennu@users.noreply.github.com>
2025-10-07 19:17:41 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,9 @@
 import sys
 
-if len(sys.argv) < 2:
-    print("⚠️ No name provided!")
-else:
-    name = sys.argv[1]
-    print(f"👋 Hello {name} from composite Python action! 🚀")
+print(f"👋 Hello Gopall Your'e running Jenkins 🚀")
+# if len(sys.argv) < 2:
+#     print("⚠️ No name provided!")
+# else:
+#     name = sys.argv[1]
+#     print(f"👋 Hello Gopall from composite Python action! 🚀")
 
